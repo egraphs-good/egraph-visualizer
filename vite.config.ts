fed1f63e98c596a37251669109bef0b3595ea1cd
Saul Shanabrook <s.shanabrook@gmail.com>
2024-08-15 14:49:44 -0400

    Add support for building as an ES lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/App.tsx'),
+      formats: ['es', ],
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime', 'react-dom'],
+    },
+  },
+
 })
