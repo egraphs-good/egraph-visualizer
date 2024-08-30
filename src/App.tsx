6092204c2146901cda4a7403f1a89b1ee1b4363d
Saul Shanabrook <s.shanabrook@gmail.com>
2024-08-30 12:54:55 -0400

    Add github repo link

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,22 @@ import DefaultCode from "/examples/manual/homepage.json?raw";
 function App() {
   const [egraph, setEgraph] = useState(DefaultCode);
   return (
-    <div className="flex min-h-screen">
-      <div className="flex w-1/3 resize-x overflow-auto">
-        <Monaco code={egraph} setCode={setEgraph} />
-      </div>
+    <>
+      <div className="flex min-h-screen">
+        <div className="flex w-1/3 resize-x overflow-auto">
+          <Monaco code={egraph} setCode={setEgraph} />
+        </div>
 
-      <div className="flex w-2/3">
-        <Visualizer egraph={egraph} />
+        <div className="flex w-2/3">
+          <Visualizer egraph={egraph} />
+        </div>
       </div>
-    </div>
+      <footer className="p-2 fixed bottom-0 min-w-full text-xs text-gray-500 text-right dark:text-gray-400">
+        <a href="https://github.com/saulshanabrook/egraph-visualizer" target="_blank" className="hover:underline">
+          github.com/saulshanabrook/egraph-visualizer
+        </a>
+      </footer>
+    </>
   );
 }
 
