ff485fbde05de26136af5b9e5c14a0f24583e6cb
Saul Shanabrook <s.shanabrook@gmail.com>
2024-09-19 16:53:24 -0400

    Try adding css to package export

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,6 @@ export default defineConfig({
         anywidget: resolve(__dirname, "src/anywidget.tsx"),
         dom: resolve(__dirname, "src/dom.tsx"),
       },
-      output: {
-        intro: 'import "./index.css";',
-      },
     },
   },
 
