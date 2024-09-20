95e7e541b849ac278c8b2d9c9900cdb2f7ca31a6
Saul Shanabrook <s.shanabrook@gmail.com>
2024-09-19 20:38:40 -0400

    Try subpath for loading on esm.sh

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "./dist/",
   build: {
     assetsInlineLimit: (path) => !path.includes("elk-worker"),
     lib: {
