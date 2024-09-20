26a5425eab263a106b33de494a3d06037632946d
Saul Shanabrook <s.shanabrook@gmail.com>
2024-09-19 20:43:41 -0400

    Another try

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./dist/",
+  base: "./",
   build: {
     assetsInlineLimit: (path) => !path.includes("elk-worker"),
     lib: {
