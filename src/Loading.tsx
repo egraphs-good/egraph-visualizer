65ca6ea859c4d22ef383c2d8d6b8bea2f57bd3f8
Saul Shanabrook <s.shanabrook@gmail.com>
2024-10-12 10:39:03 -0400

    Fix stroke width name

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -5,7 +5,7 @@ export function Loading() {
       <div className="flex items-center">
         <span className="text-3xl mr-4">Loading</span>
         <svg className="animate-spin h-8 w-8 text-gray-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
           <path
             className="opacity-75"
             fill="currentColor"
